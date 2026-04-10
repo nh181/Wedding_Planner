4ed62619e28b245100740f49412086923c9356f7
nh181 <hildebrandt.90@web.de>
2026-04-10 20:38:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hochzeitsplaner-v21';
+const CACHE = 'hochzeitsplaner-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
